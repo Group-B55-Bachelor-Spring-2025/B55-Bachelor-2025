1091a36a8176427b90603a5cdf737016eae4385b
Rana Kelani <rana.kelani@gmail.com>
2025-04-21 17:28:49 +0200

    Implement the core provider management functionality

--- src/provider-management/providers/providers.controller.ts
+++ src/provider-management/providers/providers.controller.ts
@@ -1,34 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
+import { Provider } from './entities/provider.entity';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
   @Post()
-  create(@Body() createProviderDto: CreateProviderDto) {
-    return this.providersService.create(createProviderDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body() createProviderDto: CreateProviderDto,
+  ): Promise<Provider> {
+    return await this.providersService.create(createProviderDto);
   }
 
   @Get()
-  findAll() {
-    return this.providersService.findAll();
+  async findAll(): Promise<Provider[]> {
+    return await this.providersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.providersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.providersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
-    return this.providersService.update(+id, updateProviderDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateProviderDto: UpdateProviderDto,
+  ) {
+    return await this.providersService.update(+id, updateProviderDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.providersService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.providersService.remove(+id);
   }
 }
