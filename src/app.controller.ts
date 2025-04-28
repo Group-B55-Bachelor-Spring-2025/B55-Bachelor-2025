b04f19eb5ae0a5490127095ac746aad9f53a5c69
Rana Kelani <rana.kelani@gmail.com>
2025-04-29 00:49:43 +0200

    Add EJS templating support and add initial routes, pages and layouts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard)
+  @Render('pages/index')
+  getHello() {
+    return { message: this.appService.getHello(), title: 'Home' };
+  }
+
+  @Get('login')
+  loginPage(@Req() req: Request, @Res() res: Response) {
+    if (req.cookies && req.cookies['accessToken']) {
+      return res.redirect('/');
+    }
+    return res.render('pages/auth/login', { title: 'Login' });
+  }
+
+  @Get('register')
+  registerPage(@Req() req: Request, @Res() res: Response) {
+    if (req.cookies && req.cookies['accessToken']) {
+      return res.redirect('/');
+    }
+    return res.render('pages/auth/register', { title: 'Register' });
   }
 }
