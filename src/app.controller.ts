ac00ecfadf802b653ee2bbed325fba0317ef5603
Rana Kelani <rana.kelani@gmail.com>
2025-04-29 05:09:01 +0200

    Add regions support to new address form

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,12 +5,14 @@ import { Request, Response } from 'express';
 import { AddressesService } from './location-management/addresses/addresses.service';
 import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { User } from './users/entities/user.entity';
+import { RegionsService } from './location-management/regions/regions.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly addressesService: AddressesService,
+    private readonly regionsService: RegionsService,
   ) {}
 
   @Get()
@@ -18,7 +20,8 @@ export class AppController {
   @Render('pages/index')
   async getIndex(@CurrentUser() user: User) {
     const addresses = await this.addressesService.findAll(user.role, user.id);
-    return { addresses, title: 'Home' };
+    const regions = await this.regionsService.findAll();
+    return { addresses, regions, title: 'Home' };
   }
 
   @Get('login')
