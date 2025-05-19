e74ea6353781fd73d26cca758e1fb1004ea373bb
Rana Kelani <rana.kelani@gmail.com>
2025-05-19 15:58:50 +0200

    Update database config

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -13,11 +13,11 @@ config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.PGHOST || process.env.DB_HOST,
+  port: parseInt(process.env.PGPORT || process.env.DB_PORT || '5432', 10),
+  username: process.env.PGUSER || process.env.DB_USER,
+  password: process.env.PGPASSWORD || process.env.DB_PASS,
+  database: process.env.PGDATABASE || process.env.DB_NAME,
   entities: [
     Provider,
     ProviderCredential,
